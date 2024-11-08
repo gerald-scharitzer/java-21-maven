8d050bc9c4da68575bc3a027459a5c56bea4ad6b
gerald-scharitzer <gerald.scharitzer@gmail.com>
2024-11-08 23:57:57 +0100

    runnable

--- src/main/java/net/scharitzer/Main.java
+++ src/main/java/net/scharitzer/Main.java
@@ -1,13 +1,25 @@
 package net.scharitzer;
 
+import java.io.InputStream;
+import java.io.PrintStream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main implements Runnable {
+
+    private InputStream stdin = System.in;
+    private PrintStream stdout = System.out;
+
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Main instance = new Main();
+        instance.run();
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+    }
+
+    public void run() {
+        stdout.println("run");
     }
 }
